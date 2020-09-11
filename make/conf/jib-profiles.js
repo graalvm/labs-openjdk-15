4509b7292b9643737a7502c80c7e61875f7536d6
Doug Simon <doug.simon@oracle.com>
2020-09-11 09:57:11 +0200

    enable graal and aot modules in aarch64 build (JDK-8252709)

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -454,8 +454,6 @@ var getJibProfilesProfiles = function (input, common, data) {
             dependencies: ["devkit", "gtest", "build_devkit", "pandoc"],
             configure_args: [
                 "--openjdk-target=aarch64-linux-gnu",
-		"--disable-jvm-feature-graal",
-		"--disable-jvm-feature-aot",
             ],
         },
 
